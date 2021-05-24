e1e2d114748120de99dca02b849c0d9ed4a3fa72
SantiagoBtc6 <84112242+SantiagoBtc6@users.noreply.github.com>
2021-05-24 10:44:38 -0500

    Create success.php

---
+++ codeingniter/app/Views/success.php
@@ -0,0 +1,12 @@
+< html >
+< cabeza > 
+</ cabeza >
+< cuerpo >
+< div class = "contenedor" > 
+    < div class = "row text-center" > 
+        < div class = "col-sm-6 col-sm-offset-3" > 
+        < Br > < br > < h2 estilo = "color: # 0fad00" > éxito </ h2 >  
+        </ div >
+    </ div >
+</ cuerpo >
+</ html >
